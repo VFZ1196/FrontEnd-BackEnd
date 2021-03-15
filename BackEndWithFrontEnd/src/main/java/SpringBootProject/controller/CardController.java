0127245c52444fc02987c3e588148e3607bf3331
arjun <arjunyadav102030@gmail.com>
2021-03-15 18:09:43 +0530

    first commit

--- BackEndWithFrontEnd/src/main/java/SpringBootProject/controller/CardController.java
+++ BackEndWithFrontEnd/src/main/java/SpringBootProject/controller/CardController.java
@@ -1,8 +1,10 @@
 package SpringBootProject.controller;
 
 import java.util.List;
+import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +18,12 @@ import SpringBootProject.service.CardService;
 @RestController
 public class CardController {
 	
+	Random random = new Random();
+	public static double getRandomNumber(double min, double max) {
+		double r = (int)(Math.random()*(max-min+1)+min);
+		return r;
+	}
+	
 	@Autowired
 	public CardRepository cardRepository;
 	
@@ -27,7 +35,7 @@ public class CardController {
 	public Card add(@RequestBody Card card) {
 		return cardRepository.save(card);
 	}
-	
+ 	
 	@GetMapping("/card")
 	public List<Card> getAll() {
 		return cardRepository.findAll();
@@ -38,4 +46,20 @@ public class CardController {
 		return cardRepository.findById(cardId).get();
 	}
   
+	@GetMapping("/cardValidation/{cardId}")
+	public ResponseEntity<Card> cardValidation(@PathVariable int cardId){
+		Card card = cardRepository.findById(cardId).get();
+		System.out.println(card);
+		return null;
+	}
+	
+	@PostMapping("/otp")
+	public ResponseEntity<Card> enterOtp(){
+		
+		int otpNumber = (int) getRandomNumber(1000,4000);
+		System.out.println(otpNumber);
+		
+		return null;
+		
+	}
 }
--- BackEndWithFrontEnd/src/main/java/SpringBootProject/model/Card.java
+++ BackEndWithFrontEnd/src/main/java/SpringBootProject/model/Card.java
@@ -83,4 +83,5 @@ public class Card {
 		this.details = details;
 	}
 	
+	
 }
